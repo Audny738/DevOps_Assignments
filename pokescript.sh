1f4dc53742b381cc87ae64edd2d533105e5e9ec0
Desiree Correa <audny.correac@gmail.com>
2023-01-31 19:55:40 -0600

    get id

--- pokescript.sh
+++ pokescript.sh
@@ -5,7 +5,9 @@ if [[ -z "$pokemon" ]]; then
     echo "No parameter passed"
     exit 22
 elif  wget --spider https://pokeapi.co/api/v2/pokemon/${pokemon} 2>/dev/null; then
-    curl https://pokeapi.co/api/v2/pokemon/${pokemon} | jq '{id: .id, name: .name, height: .height, weight: .weight, order: .order}'
+    #curl https://pokeapi.co/api/v2/pokemon/${pokemon} | jq '{id: .id, name: .name, height: .height, weight: .weight, order: .order}'
+    id=$(curl https://pokeapi.co/api/v2/pokemon/${pokemon} | jq -r ".id")
+    echo "id:" $id
 else
     echo "Such pokemon doesn't exists"
     exit 22
