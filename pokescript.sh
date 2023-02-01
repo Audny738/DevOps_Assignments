dfee4c80e9664a4a62ff4bdad1db95e0f108babb
Desiree Correa <audny.correac@gmail.com>
2023-01-31 20:59:35 -0600

    saved variables

--- pokescript.sh
+++ pokescript.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 pokemon=$1
-
 if [[ -z "$pokemon" ]]; then
     echo "No parameter passed"
     exit 22
-elif  wget --spider https://pokeapi.co/api/v2/pokemon/${pokemon} 2>/dev/null; then
+elif wget --spider https://pokeapi.co/api/v2/pokemon/${pokemon} 2>/dev/null; then
     #curl https://pokeapi.co/api/v2/pokemon/${pokemon} | jq '{id: .id, name: .name, height: .height, weight: .weight, order: .order}'
-    id=$(curl https://pokeapi.co/api/v2/pokemon/${pokemon} | jq -r ".id")
-    echo "id:" $id
+    url=https://pokeapi.co/api/v2/pokemon/${pokemon}
+    id=$(curl --silent --show-error ${url} | jq -r ".id")
+    name=$(curl --silent --show-error ${url} | jq -r ".name")
+    height=$(curl --silent --show-error ${url} | jq -r ".height")
+    weight=$(curl --silent --show-error ${url} | jq -r ".weight")
+    order=$(curl --silent --show-error ${url} | jq -r ".order")
+    echo "id=${id}, name=${name}, height=${height}, weight=${weight}, order=${order}"
 else
     echo "Such pokemon doesn't exists"
     exit 22
